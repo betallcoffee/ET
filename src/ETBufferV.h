9e9d4537bb5013739b8feb32202c81eae6c44290
betallcoffee <liangliang0918@126.com>
2012-11-05 16:50:59 +0800

    Complete the code of ETBufferV class.

---
+++ src/ETBufferV.h
@@ -0,0 +1,50 @@
+// Use of this source code is governed by a BSD-style license
+//  that can be found in the License file.
+//
+//  Author: betallcoffee
+//
+
+#ifndef ETBUFFERV_H
+#define ETBUFFERV_H
+
+#include <vector>
+
+namespace ET
+{
+
+    //
+    // A buffer that is based on std::vector.
+    //
+    class ETBufferV
+    {
+    public:
+        ETBufferV();
+        ~ETBufferV();
+        
+        char *beginRead() { return begin() + readIndex_; }
+        char *beginWrite() { return begin() + writeIndex_; }
+        size_t readableBytes() { return writeIndex_ - readIndex_; }
+        size_t writeableBytes() { return buffer_.size() - writeIndex_; }
+        size_t prependableBytes() { return readIndex_; }
+
+        size_t append(const char *data, size_t n);
+        const char *findCRLF();
+        const char *findCRLF(char *start);
+
+    private:
+        static const char kCRLF[];
+
+        char *begin() { return &*buffer_.begin(); }
+        //const char *begin() { return &*buffer_.begin(); }
+        void ensureWriteable(size_t n);
+        void expend(size_t size);
+        
+        std::vector<char> buffer_;
+        size_t readIndex_;
+        size_t writeIndex_;
+
+    }; // end class ETBufferV
+
+} // end namespace ET
+
+#endif // end ETBUFFERV_H
