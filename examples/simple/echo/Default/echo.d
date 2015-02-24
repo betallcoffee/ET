cdca62adfdd8e85bf8ac5b852129959d928f3f10
liangliang <liangliang0918@126.com>
2015-02-25 00:28:18 +0800

    create examples/simple/echo cdt project files.

---
+++ examples/simple/echo/Default/echo.d
@@ -0,0 +1,15 @@
+echo.d: ../echo.cpp ../EchoServer.h \
+  /Users/liang/Workspace/projects/ET/src/EventLoop.h \
+  /Users/liang/Workspace/projects/ET/src/Config.h \
+  /Users/liang/Workspace/projects/ET/src/Select.h \
+  /Users/liang/Workspace/projects/ET/src/KqueueSelect.h
+
+../EchoServer.h:
+
+/Users/liang/Workspace/projects/ET/src/EventLoop.h:
+
+/Users/liang/Workspace/projects/ET/src/Config.h:
+
+/Users/liang/Workspace/projects/ET/src/Select.h:
+
+/Users/liang/Workspace/projects/ET/src/KqueueSelect.h:
